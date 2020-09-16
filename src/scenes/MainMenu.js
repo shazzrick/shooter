39ba96b4715103a6a49d95c2bad15ef8545fd812
tzvaita <tzvaita@gmail.com>
2020-09-16 15:25:01 +0200

    Change assets folder path

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -6,17 +6,17 @@ export default class MainMenu extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('sprBg0', 'assets/sprBg0.png');
-    this.load.image('sprBg1', '..src/assets/sprBg1.png');
-    this.load.image('sprBtnPlay', '..src/assets/sprBtnPlay.png');
-    this.load.image('sprBtnPlayHover', '..src/assets/sprBtnPlayHover.png');
-    this.load.image('sprBtnPlayDown', '..src/assets/sprBtnPlayDown.png');
-    this.load.image('sprBtnRestart', '..src/assets/sprBtnRestart.png');
-    this.load.image('sprBtnRestartHover', '..src/assets/sprBtnRestartHover.png');
-    this.load.image('sprBtnRestartDown', '..src/assets/sprBtnRestartDown.png');
+    this.load.image('sprBg0', '../src/assets/sprBg0.png');
+    this.load.image('sprBg1', '../src/assets/sprBg1.png');
+    this.load.image('sprBtnPlay', '../src/assets/sprBtnPlay.png');
+    this.load.image('sprBtnPlayHover', '../src/assets/sprBtnPlayHover.png');
+    this.load.image('sprBtnPlayDown', '../src/assets/sprBtnPlayDown.png');
+    this.load.image('sprBtnRestart', '../src/assets/sprBtnRestart.png');
+    this.load.image('sprBtnRestartHover', '../src/assets/sprBtnRestartHover.png');
+    this.load.image('sprBtnRestartDown', '../src/assets/sprBtnRestartDown.png');
 
-    this.load.audio('sndBtnOver', '..src/assets/sndBtnOver.wav');
-    this.load.audio('sndBtnDown', '..src/assets/sndBtnDown.wav');
+    this.load.audio('sndBtnOver', '../src/assets/sndBtnOver.wav');
+    this.load.audio('sndBtnDown', '../src/assets/sndBtnDown.wav');
   }
 
   create() {
