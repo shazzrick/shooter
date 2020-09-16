28e2bde46dc1ed007f29d016f5e9dc75f95cb302
tzvaita <tzvaita@gmail.com>
2020-09-16 12:14:03 +0200

    Configure file systems

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+/* eslint-disable no-use-before-define */
+import Phaser from 'phaser';
+import logoImg from './assets/logo.png';
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
+  parent: 'phaser-example',
   width: 800,
   height: 600,
   scene: {
-    preload: preload,
-    create: create
-  }
+    preload,
+    create,
+  },
 };
 
+// eslint-disable-next-line no-unused-vars
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image('logo', logoImg);
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const logo = this.add.image(400, 150, 'logo');
 
   this.tweens.add({
     targets: logo,
     y: 450,
     duration: 2000,
-    ease: "Power2",
+    ease: 'Power2',
     yoyo: true,
-    loop: -1
+    loop: -1,
   });
 }
